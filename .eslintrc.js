65bfbe6b292b96fed17e7a79518027c26e88bef7
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-31 08:30:45 +0300

    sgir

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,18 @@ module.exports = {
   },
   'extends': 'eslint:recommended',
   'rules': {
-    'quotes': ['error', 'single'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': 'error',
     'comma-dangle': ['error', 'always-multiline'],
+    'complexity': [0, 11],
+    'curly': [2, 'multi-line'],
+    'eqeqeq': [2, 'allow-null'],
+    'indent': ['error', 2],
     'no-console': ['off'],
+    'object-shorthand': ['warn', 'always'],
     'prefer-arrow-callback': ['warn'],
+    'prefer-const': ['error'],
+    'quotes': ['error', 'single'],
+    'sort-imports': 'warn',
   }
 };
